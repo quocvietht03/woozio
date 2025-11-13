2bb960b1b2bb4e9ecb98d8886e5a434b01e65621
tomdevvn90 <tomdevvn90@gmail.com>
2025-11-13 07:41:11 +0700

    update dot slider product on moible

--- assets/js/main.js
+++ assets/js/main.js
@@ -183,6 +183,10 @@
 				nextEl: '.swiper-button-next',
 				prevEl: '.swiper-button-prev',
 			},
+			pagination: {
+				el: '.swiper-pagination',
+				clickable: true,
+			},
 			breakpoints: {
 				0: {
 					slidesPerView: 1,
