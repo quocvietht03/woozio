d71bb3db8a672833c5df6ab51bf0d38dabcab7ef
hieuhuynh <hieuhuynh.bearsthemes@gmail.com>
2026-02-12 10:37:04 +0700

    update slider single product

--- framework/widgets/frontend.js
+++ framework/widgets/frontend.js
@@ -2345,6 +2345,7 @@
 					loopedSlides: 5,
 					watchSlidesVisibility: true,
 					watchSlidesProgress: true,
+					threshold: 5,
 					breakpoints: {
 						0: {
 							slidesPerView: 'vertical' == thumbDirection ? 'auto' : 3,
